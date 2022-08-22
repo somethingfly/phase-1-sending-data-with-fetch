979b34f065c55500e0f2236d7f4ce8e1a0879746
Alfred <somethingfly@somethingfly.com>
2022-08-21 19:28:57 -0700

    commit tree 6625d25d0caf7471cbc5b3cacf2113237614189c

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(username,userEmail) {
+  const bodyArea = document.querySelector('body');
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: username,
+      email: userEmail
+    })
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(data => {
+    bodyArea.append(data.id);
+  })
+  .catch(error => {
+    bodyArea.append(error.message);
+  });
+}
+
+submitData("username","useremail");
